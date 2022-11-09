b50c84404a6f4f350fa470f685990e0be25a5e5a
Bogdan Popa <bogdan@defn.io>
2022-11-09 20:19:43 +0200

    test,build: add rackunit-lib to deps

--- lua-test/info.rkt
+++ lua-test/info.rkt
@@ -1,6 +1,8 @@
 #lang info
 
 (define collections "tests")
-(define deps '("base" "lua-lib"))
+(define deps '("base"
+               "lua-lib"
+               "rackunit-lib"))
 (define build-deps '("rackunit-lib"))
 (define implies '("lua-lib"))
