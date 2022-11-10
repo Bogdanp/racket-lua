43f04fae66b98066cced48096b2f1369fce1e9ac
Bogdan Popa <bogdan@defn.io>
2022-11-10 22:19:49 +0200

    emacs: update .dir-locals for #%let/cc change

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
-(("lua-lib/lang/compiler.rkt" . ((nil . ((eval . (cl-dolist (s '("#%cond" "#%define" "#%dynamic-wind" "#%lambda" "#%let/ec" "#%let" "#%unless" "#%when"))
+(("lua-lib/lang/compiler.rkt" . ((nil . ((eval . (cl-dolist (s '("#%cond" "#%define" "#%dynamic-wind" "#%lambda" "#%let/cc" "#%let" "#%unless" "#%when"))
                                                    (put (intern s) 'racket-indent-function #'defun))))))))
