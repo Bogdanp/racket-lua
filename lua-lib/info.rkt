f258562b9230a8fbfdc72d222e30fd2bf9edfbeb
Bogdan Popa <bogdan@defn.io>
2022-11-13 13:59:03 +0200

    build: use `module-suffixes' to instruct raco to compile stdlib mods

--- lua-lib/info.rkt
+++ lua-lib/info.rkt
@@ -2,16 +2,5 @@
 
 (define collection "lua")
 (define version "0.1")
-(define deps '("base"
-               "sandbox-lib"))
-(define compile-include-files
-  '("stdlib/coroutine.lua"
-    "stdlib/file.lua"
-    "stdlib/io.lua"
-    "stdlib/math.lua"
-    "stdlib/os.lua"
-    "stdlib/racket.lua"
-    "stdlib/string.lua"
-    "stdlib/table.lua"))
-(define raco-commands
-  '(("lua" (submod lua/cli main) "run lua scripts" #f)))
+(define deps '("base" "sandbox-lib"))
+(define raco-commands '(("lua" (submod lua/cli main) "run lua scripts" #f)))
