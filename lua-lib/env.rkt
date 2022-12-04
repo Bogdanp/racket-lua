46f8a41512402da9c100b8c832edc270cb7bf62e
Bogdan Popa <bogdan@defn.io>
2022-12-04 14:47:29 +0200

    env: fix current-standard-library-modules contract

--- lua-lib/env.rkt
+++ lua-lib/env.rkt
@@ -9,4 +9,9 @@
   [make-initial-environment (-> table?)]
   [current-racket-imports-enabled? (parameter/c boolean?)]
   [current-global-environment (parameter/c table?)]
-  [current-standard-library-modules (parameter/c (listof (cons/c bytes? (or/c module-path? resolved-module-path? module-path-index?))))]))
+  [current-standard-library-modules (parameter/c
+                                     (listof
+                                      (cons/c bytes? (or/c module-path?
+                                                           resolved-module-path?
+                                                           module-path-index?
+                                                           (-> table? bytes? void?)))))]))
