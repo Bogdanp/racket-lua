2a50dbcfa35f77e713f6be9823f4ba5712021f75
Bogdan Popa <bogdan@defn.io>
2022-12-12 10:09:34 +0200

    build: add license to info.rkt files

--- lua-lib/info.rkt
+++ lua-lib/info.rkt
@@ -1,5 +1,6 @@
 #lang info
 
+(define license 'BSD-3-Clause)
 (define collection "lua")
 (define version "0.1")
 (define deps '("base"
--- lua/info.rkt
+++ lua/info.rkt
@@ -1,5 +1,6 @@
 #lang info
 
+(define license 'BSD-3-Clause)
 (define collection "lua")
 (define deps '("base"
                "lua-lib"))
