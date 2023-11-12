c857403c3e8004973726ed7c148be34c7a687920
Bogdan Popa <bogdan@defn.io>
2023-11-12 22:20:33 +0200

    build: bump version

--- lua-lib/info.rkt
+++ lua-lib/info.rkt
@@ -2,7 +2,7 @@
 
 (define license 'BSD-3-Clause)
 (define collection "lua")
-(define version "0.2.1")
+(define version "0.3")
 (define deps '("base"
                "iso-printf-lib"
                "sandbox-lib"))
