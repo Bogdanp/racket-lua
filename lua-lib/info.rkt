d0b605092f76eca4098779b1ec4c9b510a55954b
Bogdan Popa <bogdan@defn.io>
2025-04-26 20:42:50 +0300

    build: bump patch version

--- lua-lib/info.rkt
+++ lua-lib/info.rkt
@@ -2,7 +2,7 @@
 
 (define license 'BSD-3-Clause)
 (define collection "lua")
-(define version "0.4.1")
+(define version "0.4.2")
 (define deps '("base"
                "iso-printf-lib"
                "sandbox-lib"))
